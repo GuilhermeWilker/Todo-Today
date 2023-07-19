60aa0c0b82c2a03b8d222ce6d2ced7050a8e196d
Guilherme Wilker <wilkerguilherme0@gmail.com>
2023-07-19 18:32:41 -0300

    refactor: directory

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,5 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
 
-import { SUPABASE_KEY, SUPABASE_URL } from "../../env.js";
+import { SUPABASE_KEY, SUPABASE_URL } from "./env.js";
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
