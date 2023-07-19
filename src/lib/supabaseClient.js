a5ed31a755b04300f2c29b2dd0eb9f422751e28d
Guilherme Wilker <wilkerguilherme0@gmail.com>
2023-07-19 19:32:19 -0300

    chore: enviorment config

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,9 +1,4 @@
 import { createClient } from "@supabase/supabase-js";
-// import dotenv from "dotenv";
-
-// dotenv.config();
-
-// import { SUPABASE_KEY, SUPABASE_URL } from "./env.js";
 
 export const supabase = createClient(
   import.meta.env.VITE_SUPABASE_URL,
